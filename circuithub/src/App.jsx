7a518ec5fcd6e4648a87c5b642d119fadd381ada
Gajraj Singh <gajrajsingh98847@gmail.com>
2025-08-26 22:13:43 +0530

    update: latest changes

--- circuithub/src/App.jsx
+++ circuithub/src/App.jsx
@@ -13,27 +13,10 @@ function App() {
 
       {/* Page Content */}
       <main className="p-6">
-        <h2 className="text-3xl font-bold text-gray-800 mb-4">Welcome to MegaMart 🛒</h2>
+        <h2 className="text-3xl font-bold text-gray-800 mb-4 bg-slate-500">Welcome to MegaMart 🛒</h2>
         <p className="text-gray-600">Your one-stop shop for everything!</p>
       </main>
-
-
-      <div className="flex h-screen items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-white">
-        Hello Tailwind v3.4.17 🚀
-      </h1>
-    </div>
     
-
-    <div className="bg-red-500 text-white text-3xl font-bold h-screen flex items-center justify-center">
-      Tailwind Works 🚀
-    </div>
-  
-export default App
-<div className="bg-purple-600 text-white text-4xl font-bold h-screen flex items-center justify-center">
-      Tailwind Fixed 🎉
-    </div>
-
     </div>
     
   )

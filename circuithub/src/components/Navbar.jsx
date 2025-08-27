8a26284014a2f24c77bba62fd53d180e7db2fed8
Gajraj Singh <gajrajsingh98847@gmail.com>
2025-08-27 21:59:05 +0530

    update: latest changes

--- circuithub/src/components/Navbar.jsx
+++ circuithub/src/components/Navbar.jsx
@@ -5,28 +5,30 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className=" text-red-400 shadow-lg sticky top-0 z-50">
+    <nav className=" text-blue-700 shadow-lg sticky top-0 z-50 flex-row bg-white">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        
         {/* Logo */}
-        <h1 className="text-2xl font-bold text-purple-400">MegaMart</h1>
+        <h1 className="text-2xl font-bold text-rgba(0, 142, 204, 1) flex">
+          CircuitHUB
+          
+        </h1>
 
         {/* Search Bar */}
-        <div className="hidden md:flex items-center bg-gray-800 rounded-lg overflow-hidden">
+        <div className="hidden md:flex items-center bg-blue-100 rounded-lg overflow-hidden w-">
           <input
             type="text"
             placeholder="Search for products..."
-            className="px-4 py-2 bg-gray-800 text-white outline-none w-72"
+            className="px-4 py-2 bg-blue-100 text-white outline-none w-50"
           />
-          <button className="bg-purple-600 px-4 py-2">
+          <button className="bg-blue-100 px-4 py-2">
             <FiSearch />
           </button>
         </div>
 
         {/* Icons */}
         <div className="flex items-center gap-5">
-          <FiUser className="cursor-pointer text-xl hover:text-purple-400" />
-          <FiShoppingCart className="cursor-pointer text-xl hover:text-purple-400" />
+          <FiUser className="cursor-pointer text-xl hover:text-purple-400 text-blue-700" />
+          <FiShoppingCart className="cursor-pointer text-xl hover:text-purple-400 text-blue-700" />
 
           {/* Mobile Menu Button */}
           <button
@@ -37,18 +39,6 @@ export default function Navbar() {
           </button>
         </div>
       </div>
-
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <div className="md:hidden bg-gray-800 px-6 py-4">
-          <ul className="space-y-3">
-            <li className="hover:text-purple-400 cursor-pointer">Home</li>
-            <li className="hover:text-purple-400 cursor-pointer">Categories</li>
-            <li className="hover:text-purple-400 cursor-pointer">Deals</li>
-            <li className="hover:text-purple-400 cursor-pointer">Contact</li>
-          </ul>
-        </div>
-      )}
     </nav>
   );
 }
